07621c51fc29f04775196072eebfc39f309ce3b1
Daniel Esposito <danielce90@gmail.com>
2018-07-02 21:43:33 +1000

    TimeStampedModel, Keyword models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class TimeStampedModel(models.Model):
+    """
+    Represents a time stamped model updating the modification date everytime
+    an instance is saved.
+    """
+    class Meta:
+        abstract = True
+        ordering = ['-creation_date']
+
+    creation_date = models.DateField(
+        default=datetime.date.today,
+        verbose_name='Creation date',
+    )
+    modification_date = models.DateField(
+        default=datetime.date.today,
+        verbose_name='Modification date',
+    )
+
+    def save(self, *args, **kwargs):
+        self.modification_date = datetime.date.today()
+        return super().save(*args, **kwargs)
+
+
+class Keyword(TimeStampedModel):
+    """Keywords that can be associated with abstract submissions."""
+    text = models.CharField(
+        max_length=128,
+        null=False,
+        blank=False,
+        default=None,
+        unique=True,
+    )
