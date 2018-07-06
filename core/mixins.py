2934aef929c0447d2e1d567839821e05cc64e805
daniel <danielce90@gmail.com>
2018-07-06 20:19:12 +1000

    abstracted mixins to handle lists

--- core/mixins.py
+++ core/mixins.py
@@ -37,3 +37,37 @@ class SetUserOnSaveMixin:
         this_user = getattr(self, self.user_kwarg, None)
         setattr(self.instance, self.user_field, this_user)
         return super().save(commit=commit)
+
+
+class ExtraKwargsMixin:
+    """
+    Pops any kwarg in `extra_kwargs` and sets them as instance variables. If not
+    passed in via kwargs, they are set to `None`.
+    """
+    extra_kwargs = None
+    
+    def __init__(self, *args, **kwargs):
+        for kwarg in self.extra_kwargs:
+            if kwarg in kwargs:
+                value = kwargs.pop(kwarg)
+                setattr(self, kwarg, value)
+            else:
+                setattr(self, kwarg, None)
+        super().__init__(*args, **kwargs)
+
+
+class SetFieldsOnSaveMixin:
+    """
+    A common pattern where a form will have a disabled foreign key
+    field that must be set before saving. Sets any fields in `extra_fields`
+    by taking the value from `extra_kwargs`.
+    
+    Must be used with `ExtraKwargsMixin`.
+    """
+    extra_kwargs = None
+    extra_fields = None
+    
+    def save(self, commit=True):
+        for kwarg, field in zip(self.extra_kwargs, self.extra_fields):
+            setattr(self.instance, field, getattr(self, kwarg, None))
+        return super().save(commit=commit)
