d6c3f71a7c977543afd0a1df8195e2b8c3982fcd
daniel <danielce90@gmail.com>
2018-07-06 17:42:33 +1000

    SetUserOnSaveMixin mixin

--- core/mixins.py
+++ core/mixins.py
@@ -3,19 +3,37 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class UserKwargsForm:
+class UserKwargsMixin:
     """
     Pops 'user' from kwargs and sets it as instance variable 'user' if it
     exists.
     """
+    user_kwarg = None
+    
     def __init__(self, *args, **kwargs):
-        if 'user' in kwargs:
-            self.user = kwargs.pop('user')
-            if self.user is not None and not isinstance(self.user, User):
+        if self.user_kwarg in kwargs:
+            user = kwargs.pop(self.user_kwarg)
+            if user is not None and not isinstance(user, User):
                 raise TypeError("`user` must be an instance "
                                 "of '{}'. Found '{}'.".format(
-                    type(User).__name__, type(self.user).__name__,
+                    type(User).__name__, type(user).__name__,
                 ))
+            setattr(self, self.user_kwarg, user)
         else:
-            self.user = None
+            setattr(self, self.user_kwarg, None)
         super().__init__(*args, **kwargs)
+        
+        
+class SetUserOnSaveMixin:
+    """
+    A common pattern where a form will have a disabled foreign key
+    field that must be set before saving.
+    For example the owner of a scholarship in a `ModelForm`
+    """
+    user_field = None
+    user_kwarg = None
+    
+    def save(self, commit=True):
+        this_user = getattr(self, self.user_kwarg, None)
+        setattr(self.instance, self.user_field, this_user)
+        return super().save(commit=commit)
