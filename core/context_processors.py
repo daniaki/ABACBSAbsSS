2639638a508e39835144f61562a57831752b8f13
daniaki <danielce90@gmail.com>
2018-07-09 21:57:04 +1000

    added assignment statuses to request

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,5 @@
 from account.models import UserGroups
+from abstract.models import Assignment
 
 def user_groups(request):
     """Adds the user group names into the request context."""
@@ -7,4 +8,13 @@ def user_groups(request):
     context['REVIEWER_GROUP'] = UserGroups.REVIEWER.value
     context['ASSIGNER_GROUP'] = UserGroups.ASSIGNER.value
     context['CHAIR_GROUP'] = UserGroups.CONFERENCE_CHAIR.value
+    return context
+
+
+def assignment_status(request):
+    """Adds the user group names into the request context."""
+    context = dict()
+    context['PENDING'] = Assignment.STATUS_PENDING
+    context['ACCEPTED'] = Assignment.STATUS_ACCEPTED
+    context['DECLINED'] = Assignment.STATUS_REJECTED
     return context
