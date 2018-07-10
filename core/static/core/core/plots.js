170b36c7c4084cb1f3d60be48f439243028d7d11
daniel <danielce90@gmail.com>
2018-07-10 19:14:00 +1000

    Added plotly to javascript static

---
+++ core/static/core/core/plots.js
@@ -0,0 +1,81 @@
+// Forward declare jQuery"s `$` symbol
+"use strict";
+jQuery, $;
+
+
+$("document").ready(function() {
+  $.ajax({
+    url: window.location.pathname,
+    type: 'GET',
+    success: function (data) {
+      // Create pie chart for `gender`, `stage` and 'aot'
+      let keyList = ['gender', 'stage', 'aot'];
+      for(let i=0; i < keyList.length; i++) {
+        let type = keyList[i];
+        console.log(type);
+        let plotData = [{values: [], labels: [], type: 'pie'}];
+        for (let key in data[type]) {
+          if (data[type].hasOwnProperty(key)) {
+            plotData[0].values.push(parseInt(data[type][key]));
+            plotData[0].labels.push(key);
+          } else {
+            console.log("'data." + type +  " is missing own property " + key);
+            console.log(data);
+          }
+        }
+
+        let title = null;
+        let id = null;
+        if (type === 'gender') {
+          id = 'gender-plot';
+          title = 'Gender';
+        }
+        else if (type === 'aot') {
+          id = 'aot-plot';
+          title = 'Aboriginal or Torres Straight Islander'
+        }
+        else if (type === 'stage') {
+          id = 'stage-plot';
+          title = 'Career Stage';
+        }
+        else {
+          console.log("Unknown type: " + type);
+        }
+
+        let layout = {
+          title: title,
+        };
+        console.log(plotData);
+        Plotly.newPlot(id, plotData, layout);
+      }
+
+      // State plot
+      let plotData = [{x: [], y: [], type: 'bar'}];
+      let type = 'state';
+      for (let key in data[type]) {
+        if (data[type].hasOwnProperty(key)) {
+          plotData[0].y.push(parseInt(data[type][key]));
+          plotData[0].x.push(key);
+        } else {
+          console.log("'data." + type +  " is missing own property " + key);
+          console.log(data);
+        }
+      }
+      let layout = {title: 'State',};
+      console.log(plotData);
+      Plotly.newPlot('state-plot', plotData, layout);
+
+    },
+    error : function(xhr, errmsg, err) {
+      console.log(xhr.status + ": " + xhr.responseText);
+    }
+  });
+
+  window.onresize = function() {
+    Plotly.Plots.resize('stage-plot');
+    Plotly.Plots.resize('state-plot');
+    Plotly.Plots.resize('gender-plot');
+    Plotly.Plots.resize('aot-plot');
+  };
+
+});
