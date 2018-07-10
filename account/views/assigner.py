abed8f199528dee961cea5de6c8dd197e3bc4432
daniel <danielce90@gmail.com>
2018-07-10 19:14:25 +1000

    Tables rendered for assigner view

--- account/views/assigner.py
+++ account/views/assigner.py
@@ -1,12 +1,11 @@
 import logging
 
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 
 from abstract import forms as abstract_forms
-from abstract import models as abstract_models
-from abstract.models import Review, Assignment
+from abstract.models import Abstract
 
 from .. import models, mixins
 
@@ -14,12 +13,24 @@ logger = logging.getLogger('django')
 
 
 class ProfileView(LoginRequiredMixin, mixins.GroupRestrictedView,
-                  mixins.AjaxView, TemplateView):
+                  mixins.AjaxView, ListView):
     """
     Profile view for assigners to assign abstracts to reviewers. Supports
     post AJAX.
     """
     template_name = 'account/assigner_profile.html'
     group_names = (models.UserGroups.ASSIGNER,)
+    queryset = Abstract.objects.all()
+    model = Abstract
     http_method_names = ('get', 'post',)
+    
 
+class AssignView(LoginRequiredMixin, mixins.GroupRestrictedView,
+                 mixins.AjaxView, DeleteView):
+    template_name = 'account/assign.html'
+    group_names = (models.UserGroups.ASSIGNER,)
+    http_method_names = ('get', 'post',)
+    form_class = abstract_forms.AssingmentForm
+    success_url = '/profile/'
+    
+    
