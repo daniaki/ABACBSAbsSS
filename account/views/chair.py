f76afa53c08ccdaaf6929be02474e4af9a32cc62
Daniel Esposito <danielce90@gmail.com>
2018-07-15 19:55:37 +1000

    POST/GET ajax methods in chair views

--- account/views/chair.py
+++ account/views/chair.py
@@ -4,9 +4,10 @@ from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.db import transaction
+from django.contrib import messages
 
 from abstract import models as abstract_models
-from demographic.models import Gender, CareerStage, State, AboriginalOrTorres
+from demographic.utilities import compute_statistics
 
 from .. import models, mixins
 
@@ -34,61 +35,38 @@ class ProfileView(LoginRequiredMixin, mixins.GroupRestrictedView,
         return context
     
     def get_ajax(self):
-        if 'abstracts' not in self.request.GET:
-            return self.error("Missing `abstracts` key from GET data.")
-    
-        abstracts = self.request.GET.getList('abstracts', [])
+        abstracts = self.request.GET.getlist('abstracts[]', [])
         for abstract_id in abstracts:
             if not abstract_models.Abstract.objects.filter(
                     id=abstract_id).count():
                 return self.error('Could not find abstract id `{}`.'.format(
                     abstract_id
                 ))
-    
         abstracts = abstract_models.Abstract.objects.filter(pk__in=abstracts)
-        data = {'gender': {}, 'stage': {}, 'aot': {}, 'state': {}}
-        for gender in Gender.objects.all():
-            count = abstracts.filter(
-                submitter__profile__gender__text=gender.text).count()
-            data['gender'][gender.text] = count
-    
-        for cs in CareerStage.objects.all():
-            count = abstracts.filter(
-                submitter__profile__career_stage__text=cs.text).count()
-            data['stage'][cs.text] = count
-    
-        for aot in AboriginalOrTorres.objects.all():
-            count = abstracts.filter(
-                submitter__profile__aboriginal_or_torres__text=aot.text).count()
-            data['aot'][aot.text] = count
-    
-        for state in State.objects.all():
-            count = abstracts.filter(
-                submitter__profile__state__text=state.text).count()
-            data['state'][state.text] = count
-    
+        if not abstracts.count():
+            abstracts = None
+        data = compute_statistics(abstracts)
         return JsonResponse(data=data)
     
     def post_ajax(self):
-        if 'abstracts' not in self.request.POST:
-            return self.error("Missing `abstracts` key from POST data.")
-        
-        abstracts = self.request.POST.getList('abstracts', [])
+        abstracts = self.request.POST.getlist('abstracts[]', [])
         for abstract_id in abstracts:
-            if not abstract_models.Abstract.objects.filter(id=abstract_id).count():
+            if not abstract_models.Abstract.objects\
+                    .filter(id=abstract_id).count():
                 return self.error('Could not find abstract id `{}`.'.format(
                     abstract_id
                 ))
         abstracts = abstract_models.Abstract.objects.filter(pk__in=abstracts)
-        
+
         with transaction.atomic():
             for abstract in abstracts.all():
-                abstract.approved = True
+                abstract.accepted = True
                 abstract.save()
                 
             for abstract in abstract_models.Abstract.objects.all():
                 if abstract not in abstracts:
-                    abstract.approved = False
+                    abstract.accepted = False
                     abstract.save()
+            messages.success(self.request, "Selection updated.")
 
         return JsonResponse(data={'success': "Abstracts updated!"})
