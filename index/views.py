666f80278bc196b04e573323a6b3bbb0cd47cf53
Daniel Esposito <danielce90@gmail.com>
2018-07-15 19:56:20 +1000

    Refactored demographics statistics into function

--- index/views.py
+++ index/views.py
@@ -1,35 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from demographic.models import Gender, CareerStage, AboriginalOrTorres, State
 from abstract.models import Abstract
+from demographic.utilities import compute_statistics
 
 
 def index(request):
     if request.is_ajax():
-        data = {'gender': {}, 'stage': {}, 'aot': {}, 'state': {}}
         abstracts = Abstract.objects.all()
-        
-        for gender in Gender.objects.all():
-            count = abstracts.filter(
-                submitter__profile__gender__text=gender.text).count()
-            data['gender'][gender.text] = count
-    
-        for cs in CareerStage.objects.all():
-            count = abstracts.filter(
-                submitter__profile__career_stage__text=cs.text).count()
-            data['stage'][cs.text] = count
-    
-        for aot in AboriginalOrTorres.objects.all():
-            count = abstracts.filter(
-                submitter__profile__aboriginal_or_torres__text=aot.text).count()
-            data['aot'][aot.text] = count
-    
-        for state in State.objects.all():
-            count = abstracts.filter(
-                submitter__profile__state__text=state.text).count()
-            data['state'][state.text] = count
-            
+        data = compute_statistics(abstracts)
         return JsonResponse(data=data)
 
     return render(request, "index/index.html", {})
