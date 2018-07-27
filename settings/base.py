8b16b996e6757c504fc793c991140f2969d3b5f2
daniel <danielce90@gmail.com>
2018-07-27 13:05:30 +1000

    decode error fix for python3.5

--- settings/base.py
+++ settings/base.py
@@ -186,7 +186,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = str(BASE_DIR / 'static/')
 
 
 # Redirect to home URL after login (Default redirects to /profile/)
