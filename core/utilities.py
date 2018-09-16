8b72a72f57e5ea467be3df821f0dbe1b7dcbc4f0
Daniel Esposito <danielce90@gmail.com>
2018-09-17 00:24:37 +1000

    Method for getting server local closing date

---
+++ core/utilities.py
@@ -0,0 +1,8 @@
+from django.utils import timezone
+from django.utils.dateparse import parse_datetime
+from django.conf import settings
+
+
+def get_local_closing_date():
+    return parse_datetime(
+        settings.CLOSING_DATE).astimezone(tz=timezone.get_current_timezone())
