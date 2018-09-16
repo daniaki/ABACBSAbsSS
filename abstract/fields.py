68040cffbe3133d8f8c5d3c096d11fdc704f8aa7
Daniel Esposito <danielce90@gmail.com>
2018-09-17 00:26:13 +1000

    CategorySelect allowing disabled options

--- abstract/fields.py
+++ abstract/fields.py
@@ -1,6 +1,30 @@
 from django.core.exceptions import ValidationError
-from django.forms import ModelMultipleChoiceField
+from django.forms import ModelMultipleChoiceField, CheckboxSelectMultiple
 from django.utils.translation import ugettext_lazy as _
+
+
+class CategorySelect(CheckboxSelectMultiple):
+    def __init__(self, disabled_choices=(), *args, **kwargs):
+        self._disabled_choices = disabled_choices
+        super().__init__(*args, **kwargs)
+
+    @property
+    def disabled_choices(self):
+        return self._disabled_choices
+
+    @disabled_choices.setter
+    def disabled_choices(self, other):
+        self._disabled_choices = other
+
+    def create_option(self, name, value, label, selected, index,
+                      subindex=None, attrs=None):
+        option_dict = super().create_option(
+            name, value, label, selected, index,
+            subindex=subindex, attrs=attrs
+        )
+        if value in self.disabled_choices:
+            option_dict['attrs']['disabled'] = 'disabled'
+        return option_dict
 
 
 class FlexibleModelMultipleChoiceField(ModelMultipleChoiceField):
